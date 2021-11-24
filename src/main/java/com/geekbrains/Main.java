64ac0bbf757d08d698ae5f3f917f71647356248c
RazerPro <Drakula3D@mail.ru>
2021-11-24 22:45:51 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/Main.java
@@ -0,0 +1,15 @@
+package com.geekbrains;
+
+public class Main {
+    public static int SIZE = 3;
+    public static int DOTS_TO_WIN = 3;
+    public static char DOT_EMPTY = '.';
+    public static char DOT_X = 'X';
+    public static char DOT_O = 'O';
+    public static char[][] map;
+    public static void main(String[] args) {
+
+    }
+
+
+}
